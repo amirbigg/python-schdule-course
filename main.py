232dc2d8b6ff67a4f63e2854430c8987ef1f3b19
amirbigg <amirbig44@gmail.com>
2019-11-25 21:23:35 +0330

    add ep2

--- main.py
+++ main.py
@@ -2,8 +2,20 @@
 
 
 class Scheduler:
-	pass
+	def __init__(self):
+		self.jobs = []
+
+	def every(self, interval=1):
+		job = Job(interval)
+		self.jobs.append(job)
+		return job
 
 
 class Job:
-	pass
+	def __init__(self, interval):
+		self.interval = interval
+		self.job_func = None
+		self.last_run = None
+		self.next_run = None
+		self.unit = None
+		self.period = None
